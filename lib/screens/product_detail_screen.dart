49d2364e3536e25064b8d4a533bfa252f3999822
Krutika Injamuri <kruteeka.injamuri@gmail.com>
2020-06-22 11:32:29 +0530

    Adding minimal details to the product detail screen

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -12,7 +12,39 @@ class ProductDetailScreen extends StatelessWidget {
         .findById(productId);
     return Scaffold(
       appBar: AppBar(
-        title: Text(productId),
+        title: Text(loadedProduct.title),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 300,
+              width: double.infinity,
+              child: Image.network(
+                loadedProduct.imageURL,
+                fit: BoxFit.cover,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              '\$${loadedProduct.price}',
+              style: TextStyle(color: Colors.grey, fontSize: 20),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+                padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                width: double.infinity,
+                child: Text(
+                  loadedProduct.subtitle,
+                  textAlign: TextAlign.center,
+                  softWrap: true,
+                ))
+          ],
+        ),
       ),
     );
   }
