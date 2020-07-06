1d1b2f61492c7bb7ebfb2e52456ba63fdc3761ae
Krutika Injamuri <kruteeka.injamuri@gmail.com>
2020-07-06 13:18:25 +0530

    Login/Signup

--- lib/providers/auth_provider.dart
+++ lib/providers/auth_provider.dart
@@ -8,16 +8,24 @@ class AuthProvider with ChangeNotifier {
   DateTime _expiryDate;
   String _userId;
 
-  Future<void> signup(String email, String password) async {
-    const url =
-        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyA0rnfr8cifXwGQcoWARDq2kEdtRDvEh1Q';
+  Future<void> _authenticate(
+      String email, String password, String urlSegment) async {
+    final url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=AIzaSyA0rnfr8cifXwGQcoWARDq2kEdtRDvEh1Q';
 
-    final response = await http.post(url,
+    await http.post(url,
         body: json.encode({
           'email': email,
           'password': password,
           'returnSecureToken': true,
         }));
-    print(json.decode(response.body)['refreshToken']);
+  }
+
+  Future<void> signup(String email, String password) async {
+    return _authenticate(email, password, 'signUp');
+  }
+
+  Future<void> login(String email, String password) async {
+    return _authenticate(email, password, 'signInWithPassword');
   }
 }
